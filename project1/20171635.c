f2f251700810946bc1251d1c30d88b922d3233a0
sanglee <pksll325@gmail.com>
2019-03-07 17:15:36 +0900

    shell commad start

--- project1/20171635.c
+++ project1/20171635.c
@@ -1,7 +1,23 @@
-#include <"20171635.h">
+#include "20171635.h"
 
+
+void read_cmd(char *cmd);
 int main(){
+	char cmd[20];
 
+	while(1){
+		printf("sicsim> ");
+		scanf("%s", cmd);
 
-		return 0;
+		
+	}
+
+	return 0;
 }
+void read_cmd(char *cmd){
+	typedef enum cmd{
+		help, dir, quit, history,
+		dump, edit, fill, reset,
+		opcode, opcodelist
+	};
+}		
