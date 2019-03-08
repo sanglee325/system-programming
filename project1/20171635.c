dbf66a5e351cd0b427152a3dd58b2850b93d4d47
sanglee <pksll325@gmail.com>
2019-03-08 18:58:24 +0900

    added project

--- project1/20171635.c
+++ project1/20171635.c
@@ -1,6 +1,5 @@
 #include "20171635.h"
 
-
 void read_cmd(char *cmd);
 int main(){
 	char cmd[20];
@@ -8,16 +7,18 @@ int main(){
 	while(1){
 		printf("sicsim> ");
 		scanf("%s", cmd);
-
 		
 	}
 
 	return 0;
 }
 void read_cmd(char *cmd){
-	typedef enum cmd{
+	typedef enum CMD{
 		help, dir, quit, history,
 		dump, edit, fill, reset,
 		opcode, opcodelist
 	};
+
+	CMD command;
+	
 }		
