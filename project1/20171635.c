e35bcd3ab3b9e3b9460f51c158189c4bcb52e564
sanglee <pksll325@gmail.com>
2019-03-10 17:44:13 +0900

    exceptions added

--- project1/20171635.c
+++ project1/20171635.c
@@ -1,12 +1,12 @@
 #include "20171635.h"
 
 bool exit_flag = false;
-int main(){
+int main() {
 	FILE *fp = fopen("opcode.txt", "r");
 	char input_str[MAX_INPUT_LEN],
 		 *command;
 
-	while(1){
+	while(1) {
 		printf("sicsim> ");
 		//input string needs to be tokenized
 		fgets(input_str, sizeof(input_str), stdin);
@@ -17,8 +17,7 @@ int main(){
 		 * * * * * * * * * * * * * */
 
 	 	command = strtok(input_str, " ");
-		command = strtok(command, "\n");
-
+		
 		read_command(command);
 		if(exit_flag == true) break; 
 	}
@@ -27,19 +26,36 @@ int main(){
 	return 0;
 }
 
-void read_command(char *command){
-	//command about help
-	if(!strcmp(command, "help") || !strcmp(command, "h")){
-		command_help();
-	}
-	if(!strcmp(command, "dir") || !strcmp(command, "d")){
-		command_dir();
-	}
-	if(!strcmp(command, "quit") || !strcmp(command, "q")){
-		command_quit();
-	}
-	if(!strcmp(command, "history") || !strcmp(command, "hi")){
-		command_history();
+void read_command(char *command) {
+	//command about invalid inputs
+	if(!strcmp(command, "\n")) {
+		return;
 	}
 
+	command = strtok(command, "\n");
+	//command about help
+	if(!strcmp(command, "help") || !strcmp(command, "h")) {
+		command_help();
+		return;
+	}
+	//command about dir
+	if(!strcmp(command, "dir") || !strcmp(command, "d")) {
+		command_dir();
+		return;
+	}
+	//command about quit
+	if(!strcmp(command, "quit") || !strcmp(command, "q")) {
+		command_quit();
+		return;
+	}
+	//command about history
+	if(!strcmp(command, "history") || !strcmp(command, "hi")) {
+		command_history();
+		return;
+	}
+
+	else {
+		printf("ERROR: Unvaild command\n");
+		return;
+	}
 }		
