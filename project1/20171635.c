ce756c85cc4d7906afe55dfe7920dca943546948
sanglee <pksll325@gmail.com>
2019-03-23 02:05:46 -0700

    input error edited

--- project1/20171635.c
+++ project1/20171635.c
@@ -5,16 +5,16 @@ char *input_str; //command line input
 
 int main() {
 	FILE *fp = fopen("opcode.txt", "r");
-	memory = (unsigned char*)calloc(MEMORY_SIZE, sizeof(unsigned char));
+	memory = (unsigned char*)calloc((MEMORY_SIZE), sizeof(unsigned char));
 
 	init_table();
 	read_opcode(fp);
 
 	while(1) {
-		input_str = (char*)calloc(MAX_INPUT_LEN, sizeof(char));
+		input_str = (char*)calloc((MAX_INPUT_LEN), sizeof(char));
 		printf("sicsim> ");
 		
-		fgets(input_str, sizeof(input_str), stdin);
+		fgets(input_str, MAX_INPUT_LEN, stdin);
 		read_command(input_str);
 		free(input_str);
 
