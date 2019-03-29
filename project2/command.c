1ff1ea8e985ab28db82e6efef2d00c202a53030d
sanglee <pksll325@gmail.com>
2019-03-29 00:22:09 -0700

    HELP: added

--- project2/command.c
+++ project2/command.c
@@ -19,6 +19,9 @@ void command_help() {
 	printf("reset\n");
 	printf("opcode mnemonic\n");
 	printf("opcodelist\n");
+	printf("assemble filename\n");
+	printf("type filename\n");
+	printf("symbol\n");
 }
 
 /*----------------------------------------------------------*/
